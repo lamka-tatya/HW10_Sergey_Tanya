060a4b921f2e3d499ff8abb80de3f93e03c8b706
chernecova <chernecova@bars-open.ru>
2018-09-03 23:05:11 +0300

    WPF UI added

---
+++ HW10_Sergey_Tanya/StartUpWpf/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using Domain;
+using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace StartUpWpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var viewModel = (MultipleSeriesVm)DataContext;
+            var roundsCount = viewModel.RoundsCount;
+            var gamesCount = viewModel.GamesCount;
+            var series = new SeriesCollection();
+            var seriaValuesByPlayersCount = new Dictionary<int, List<Tuple<int, double>>>();
+
+            viewModel.DescriptionBlock = $"Усредненные результаты при количестве игр {gamesCount} и {roundsCount} раундов в каждой игре";
+
+            for (int currentPlayersCount = 1; currentPlayersCount < viewModel.MaxPlayersCount + 1; currentPlayersCount++)
+            {
+                var emptyValue = new List<Tuple<int, double>>();
+                seriaValuesByPlayersCount.Add(currentPlayersCount, emptyValue);
+
+                for (int currnetWipLimit = 1; currnetWipLimit <= viewModel.MaxWipLimit; currnetWipLimit++)
+                {
+                    var doneCardsCountSum = 0;
+
+                    for (int gameNum = 0; gameNum < gamesCount; gameNum++)
+                    {
+                        var game = new Game(new Board(new WipLimit((uint)currnetWipLimit)), new Coin());
+
+                        for (int currentPlayer = 0; currentPlayer < currentPlayersCount; currentPlayer++)
+                        {
+                            game.AddPlayer(new Player());
+                        }
+
+                        for (int roundNum = 0; roundNum < roundsCount; roundNum++)
+                        {
+                            game.PlayRound();
+                        }
+                       
+                        doneCardsCountSum += game.CardsThat(Status.Done).Count();
+                    }
+
+                    var doneCardsCount = (double)doneCardsCountSum / (double)gamesCount;
+
+                    seriaValuesByPlayersCount[currentPlayersCount].Add(new Tuple<int, double>(currnetWipLimit, doneCardsCount));
+                }
+            }
+
+            viewModel.Labels = seriaValuesByPlayersCount.First().Value.Select(x => x.Item1.ToString()).ToArray();
+
+            foreach (var item in seriaValuesByPlayersCount)
+            {
+                series.Add(new LineSeries
+                {
+                    Title = $"{item.Key} игроков",
+                    PointGeometry = DefaultGeometries.Square,
+                    Fill = Brushes.Transparent,
+                    Values = new ChartValues<double>(item.Value.Select(x => x.Item2))
+                });
+            }
+
+            viewModel.Series = series;
+        }
+    }
+}
---
+++ HW10_Sergey_Tanya/StartUpWpf/MultipleSeriesVm.cs
@@ -0,0 +1,80 @@
+﻿using LiveCharts;
+using System.ComponentModel;
+
+namespace StartUpWpf
+{
+    public class MultipleSeriesVm : INotifyPropertyChanged
+    {
+        private string[] _labels;
+        private int _maxPlayersCount = 5;
+        private int _maxWipLimit = 5;
+        private int _roundsCount = 20;
+        private int _gamesCount = 50;
+        private string _descriptionBlock = "Для построения графиков нажмите \"Начать игру\"";
+        private SeriesCollection _series;
+
+        public int MaxPlayersCount
+        {
+            get => _maxPlayersCount;
+            set => _maxPlayersCount = value;
+        }
+        public int MaxWipLimit
+        {
+            get => _maxWipLimit;
+            set => _maxWipLimit = value;
+        }
+
+        public int RoundsCount
+        {
+            get => _roundsCount;
+            set => _roundsCount = value;
+        }
+
+        public int GamesCount
+        {
+            get => _gamesCount;
+            set => _gamesCount = value;
+        }
+
+        public string[] Labels
+        {
+            get => _labels;
+            set
+            {
+                _labels = value;
+                OnPropertyRaised("Labels");
+            }
+        }
+
+        public string DescriptionBlock
+        {
+            get => _descriptionBlock;
+            set
+            {
+                _descriptionBlock = value;
+                OnPropertyRaised("DescriptionBlock");
+            }
+        }
+
+        public SeriesCollection Series
+        {
+            get => _series;
+            set
+            {
+                _series = value;
+                OnPropertyRaised("Series");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyRaised(string propertyname)
+        {
+            var propertyChangedEvent = PropertyChanged;
+            if (propertyChangedEvent != null)
+            {
+                propertyChangedEvent(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+    }
+}
