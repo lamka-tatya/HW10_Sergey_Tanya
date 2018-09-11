667b3e115d1dc29e0de0d186297d2eef6f60b321
chernecova <chernecova@bars-open.ru>
2018-09-11 19:06:57 +0300

    исправлен преждевременный выход из цикла

--- HW10_Sergey_Tanya/Domain/Game.cs
+++ HW10_Sergey_Tanya/Domain/Game.cs
@@ -103,7 +103,7 @@ namespace Domain
                         TryTakeNewCard(playerId) ||
                         TryUnblockCard(playerId))
                     {
-                        return;
+                        continue;
                     }
                     else
                     {
